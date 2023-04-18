41798fe44ee2c7e712aed73f0f2f22bbf606fbea
MyCode07 <erem19992015@mail.ru>
2023-04-18 09:45:42 +0300

    refresh src

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import "./utils/smoothScroll.js";
-import "./utils/home.js";
-import "./utils/menu.js";
-import "./utils/scroll.js";
-import "./utils/team.js";
-import "./utils/feedback.js";
-import "./utils/contacts.js";
-
